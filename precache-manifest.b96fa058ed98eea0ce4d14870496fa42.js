30aa73968b823dc90bc16be22eb9c98e73d4ec22
Oleg Lozovatyi <lozovatyi.jr@gmail.com>
2020-06-29 09:18:20 -0400

    Updates

---
+++ precache-manifest.b96fa058ed98eea0ce4d14870496fa42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de151be835c45234a5e938b141803fd0",
+    "url": "/aBitterParent/index.html"
+  },
+  {
+    "revision": "09639934492fbd491556",
+    "url": "/aBitterParent/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84a088fd096a2649faf7",
+    "url": "/aBitterParent/static/js/2.fa7715ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aBitterParent/static/js/2.fa7715ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09639934492fbd491556",
+    "url": "/aBitterParent/static/js/main.def3ed6c.chunk.js"
+  },
+  {
+    "revision": "bb182f778f51a88f004c",
+    "url": "/aBitterParent/static/js/runtime-main.d7286201.js"
+  }
+]);
